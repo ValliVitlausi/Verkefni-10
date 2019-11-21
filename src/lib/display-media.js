553033963a1b35f2beede4cc9e367e6da81111e5
Valgeir <valgeir@sigurdsson.me>
2019-11-21 18:14:12 +0000

    commit

--- src/lib/display-media.js
+++ src/lib/display-media.js
@@ -1,3 +1,5 @@
+import getRandomImage from "./nasa-api";
+import el from "./helpers";
 // todo vísa í rétta hluti með import
 
 // breytur til þess að halda utan um html element nodes
@@ -6,20 +8,23 @@ let text; // texti fyrir mynd á forsíðu
 let img; // mynd á forsíðu
 
 let image; // object sem inniheldur núverandi mynd á forsíðu.
-
+var json = JSON.parse(apod);
 /*
  * Sækir nýja Mynd af handahófi frá Nasa API og birtir hana á forsíðunni
  * ásamt titli og texta.
  */
 function getNewImage() {
-
+    getRandomImage();
+    img = json.hdurl;
+    title = json.title;
+    el(img);
 }
 
 /*
  * Vistar núverandi mynd í storage.
  */
 function saveCurrentImage() {
-
+     window.localStorage("")
 }
 
 /*
@@ -27,7 +32,9 @@ function saveCurrentImage() {
  *
  */
 export default function init(apod) {
-
+    document.getElementById("new-image-button").addEventListener("click", getNewImage);
+    document.getElementById("save-image-button").addEventListener("click", saveCurrentImage);
+    getNewImage();
 }
 
 /*
--- src/lib/nasa-api.js
+++ src/lib/nasa-api.js
@@ -2,9 +2,9 @@
 /**
  * Sækir Myndir frá nasa API. Til þess að sjá dæmi um json svari sjá apod.json
  */
-
+import {randomNumber} from "./helpers";
 // API lykill til að fá aðgang að nasa gögnum.
-const API_KEY = 'DEMO_KEY';
+const API_KEY = 'g88ECLOgt32aZso7MDQigWADBbk3TKm5Y45Ad4V9';
 // Slóð að sækja myndir frá. Dæmi um heila slóð https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-11-10
 const URL = 'https://api.nasa.gov/planetary/apod';
 
@@ -14,6 +14,19 @@ const URL = 'https://api.nasa.gov/planetary/apod';
  *
  * @returns {Promise} sem mun innihalda upplýsingar um mynd/myndband hjá nasa.
  */
-export default async function getRandomImage() {
-
+ export default async function getRandomImage() {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    today = mm + '-' + dd + '-' + yyyy;
+    fetch(`${URL}?${API_KEY}&date=${today}`)
+    .then((result) => {
+    if (!result.ok) {
+      throw new Error('Non 200 status');
+    }
+    return apod.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
 }
